f2372c039b1ac617048415a830e3b90e04a53fd3
Jean-Baptiste Dominguez <jeanbaptiste.dominguez@gmail.com>
2015-11-07 07:32:39 +0100

    maj front

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -185,19 +185,6 @@ angular.module('starter.controllers', [])
      */
     $scope.isLoading = false;
     
-    $scope.recherchePlaceLibre = function () {
-        
-        $rootScope.loading.show();
-        
-        Place.findFreePlaces('6.176248545363775', '48.695384785489395','1').success(function (data) {
-            $scope.places = data;
-            $rootScope.loading.hide();
-        }).error(function () {
-           $scope.places = false;
-           $rootScope.loading.hide();
-        });
-        
-    };
 }])
 .controller('ContentController', ['$scope', '$ionicSideMenuDelegate', 
     function ($scope, $ionicSideMenuDelegate) {

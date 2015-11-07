44a3e203dc583cd08eaf14a949db02e39efe30e9
Chervaliery <yoann.chevalier@mines-nancy.org>
2015-11-07 07:04:51 +0100

    First freeplace implement

--- client/www/js/controller-freeplace.js
+++ client/www/js/controller-freeplace.js
@@ -1,10 +1,22 @@
 angular.module('starter.controller-freeplace', [])
 
-.controller('FreePlaceCtrl', ['$scope', 
-    function ($scope) {
-    /**
-     * Init des variables
-     */
-    //$scope.isLoading = true;
+.controller('FreePlaceCtrl', ['$scope', '$http' 
+    function ($scope,$http) {
 
+	$scope.freePlace = function(){
+		var x = 12;
+		var y = 12;
+		var route = '/map/addPlaceInformation';
+		var payload = {
+				'x' : x,
+				'y' : y,
+				'isFree' :'true'};
+        $http.post(API_URL + route, payload).success(function (data) {
+            $scope.send = "envoyé";
+            $rootScope.loading.hide();
+        }).error(function (err) {
+        	$scope.send = "non envoyé";
+            $rootScope.loading.hide();
+        });
+	}
 }]);

12e4893198117e9b48747aa5449560f27442fad4
ChristophePhan <christophe.phan.1@gmail.com>
2015-11-08 15:43:51 +0100

    playbasis player

--- client/www/js/controllers/profileCtrl.js
+++ client/www/js/controllers/profileCtrl.js
@@ -2,5 +2,8 @@ controllers
 .controller('ProfileCtrl', ['$scope', '$rootScope', '$http', 'Place', 'Profile',
                               function ($scope, $rootScope, $http, Place, Profile) {
 	var token = Profile.app_auth("2336542779", "3a8c82795e662c25a87b692d444986bb");
-	console.log(Profile.user_info(token));
+
+  Profile.user_info(token).then(function(data){
+    $scope.player = data;
+  })
 }]);

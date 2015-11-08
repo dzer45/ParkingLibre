7df949f7c79ffbc32b5345588710cfd58264d7e8
ChristophePhan <christophe.phan.1@gmail.com>
2015-11-08 03:24:44 +0100

    affichage qualité air

--- client/www/js/rootCtrl.js
+++ client/www/js/rootCtrl.js
@@ -1,6 +1,6 @@
 controllers
-.controller('RootCtrl', ['$scope', '$rootScope','$timeout', 'Sensitize', 'AirQuality',
-    function($scope, $rootScope, $timeout, Sensitize, AirQuality) {
+.controller('RootCtrl', ['$scope', '$rootScope','$timeout', 'Sensitize', 'AirQuality','$q',
+    function($scope, $rootScope, $timeout, Sensitize, AirQuality, $q) {
       $scope.elapsed = false;
       $timeout(function(){$scope.elapsed = true}, 10000);
 
@@ -8,12 +8,18 @@ controllers
         $scope.sentence = data[0].sentence;
       });
 
-      AirQuality.getAirQuality().then(function(data){
-        AirQuality.rate = parseInt(data[0]);
-        $scope.ratings = 'AirQuality.rate'; 
+      var deferred = $q.defer();
+      deferred.resolve(AirQuality.getAirQuality())
+      deferred.promise.then(function(data){
+        var num = parseInt(data[0])
+        num = ~~(num/2);
+        console.log(num)
+        $scope.rate = new Array(num)
+        $scope.rate2 = new Array(5-num)
       });
 
 
+      // var rate = parseInt(deferred.promise.data[0])
 
 
 }]);

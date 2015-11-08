e2d9d254f852fc6734dd59b0db81870302c678b8
Chervaliery <yoann.chevalier@mines-nancy.org>
2015-11-08 02:17:47 +0100

    Page d'authentification

--- client/www/js/homeCtrl.js
+++ client/www/js/homeCtrl.js
@@ -5,7 +5,6 @@ controllers
      * Init variable
      */
     $scope.isLoading = false;
-    
 
     /**
      * Modal des parametres
@@ -22,38 +21,38 @@ controllers
         { text: "Radius", checked: false },
         { text: "Limite", checked: false }
     ];
-    $scope.logout = function() {
-    	$rootScope.user = {};
-    }
-	$scope.showLoginPopup = function() {
-		$rootScope.user = {};
+    
+   
+    $ionicModal.fromTemplateUrl('templates/modal-authentification.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.modal = modal
+        if(!$rootScope.user){
+        	$scope.modal.show();
+        }
+      });
 
-		// An elaborate, custom popup
-		var myPopup = $ionicPopup.show({
-			template:'<form novalidate class="css-form">' +
-			    'Login: <input type="text" ng-model="user.login" required /><br />'+
-			    'Password: <input type="password" ng-model="user.password" required /><br />'+
-			  '</form>',
-			title: 'Authentification',
-			scope: $rootScope,
-			buttons: [
-			          { text: 'Cancel' },
-			          {
-			        	  text: '<b>Login</b>',
-			        	  type: 'button-positive',
-			        	  onTap: function(e) {
-			                  if (!$rootScope.user.login) {
-			                      //don't allow the user to close unless he enters wifi password
-			                      e.preventDefault();
-			                    } else {
-			                    	$rootScope.user.score = 42;
-			                    	$rootScope.user.logged = true;
-			                        return $rootScope.user.login;
-			                    }
-			                }
-			          },
-			          ]
-		});
-	}
-      
+      $scope.openModal = function() {
+    	  $scope.user = null;
+    	  $rootScope.user = null;
+        $scope.modal.show();
+      };
+
+      $scope.closeModal = function() {
+        $scope.modal.hide();
+      };
+    
+    $scope.logout = function() {
+    	
+    	$scope.openModal();
+    };
+    
+    $scope.login = function(user){
+    	$rootScope.user = user;
+    	$rootScope.user.logged = true;
+    	$rootScope.user.score = 42;
+    	$scope.closeModal();
+    };
+    
 }]);
